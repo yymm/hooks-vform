130d1eebe8e384f1eb60db535ca31a5e0a9abdbf
yano <yuya.yano@origami.com>
2019-05-03 14:00:06 +0900

    change d.ts pos

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ DtsBundlePlugin.prototype.apply = function(compiler) {
     dts.bundle({
       name: "hooks-vform",
       main: rootDir + "/lib/**/*.d.ts",
-      out: rootDir + "/lib/index.d.ts",
+      out: rootDir + "/hooks-vform.d.ts",
       removeSource: true,
       outputAsModuleFolder: true
     });
